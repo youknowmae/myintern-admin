b03a46d144c1ad7654c932309c38e8dd84a4b574
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-07-18 00:45:17 +0800

    very small update

--- src/app/main/components/students/components/view/view.component.ts
+++ src/app/main/components/students/components/view/view.component.ts
@@ -8,6 +8,25 @@ import { MaterialsModules } from '../../../../../modules/materials.module';
 })
 
 export class ViewComponent {
-  binding: any
+  student: any = {
+    birth_date: "11-20-2022",
+    civil_status: "single",
+    gender: "M",
+    citizenship: "filipino",
+    religion: "Catholic",
+    student_profile: {
+      father_name: "Juan Delacruz",
+      father_employment: "worker",
+      mother_name: "Maria Delacruz",
+      mother_employment: "Housewife",
+      region: "III",
+      province: "Zambales",
+      municipality: "Olongapo",
+      barangay: "East Tapinac",
+      address: "#22 Donor Street",
+      zip_code: "2200"
+    }
+
+  }
 
 }
