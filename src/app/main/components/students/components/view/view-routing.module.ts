1aeba22cc1b5496cbc781e405f2284c502ed75c4
youknowmae <ganaangelmae@gmail.com>
2024-07-25 23:58:17 +0800

    students view

--- src/app/main/components/students/components/view/view-routing.module.ts
+++ src/app/main/components/students/components/view/view-routing.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AttendanceformComponent } from './components/attendanceform/attendanceform.component';
 import { AccomplishmentreportComponent } from './components/accomplishmentreport/accomplishmentreport.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
+import { StudentprofileComponent } from './components/studentprofile/studentprofile.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'buttons', pathMatch: 'full' },
+  { path: '', redirectTo: 'studentprofile', pathMatch: 'full' },
   { path: 'attendanceform', component: AttendanceformComponent },
   { path: 'accomplishmentreport', component: AccomplishmentreportComponent },
-  { path: 'buttons', component: ButtonsComponent },
-
+  { path: 'studentprofile', component: StudentprofileComponent },
 ];
 
 @NgModule({
--- src/app/main/components/students/components/view/view.module.ts
+++ src/app/main/components/students/components/view/view.module.ts
@@ -5,7 +5,6 @@ import { ViewRoutingModule } from './view-routing.module';
 import { StudentprofileComponent } from './components/studentprofile/studentprofile.component';
 import { AttendanceformComponent } from './components/attendanceform/attendanceform.component';
 import { AccomplishmentreportComponent } from './components/accomplishmentreport/accomplishmentreport.component';
-import { ButtonsComponent } from './components/buttons/buttons.component';
 import { MaterialsModules } from '../../../../../modules/materials.module';
 
 
@@ -14,7 +13,6 @@ import { MaterialsModules } from '../../../../../modules/materials.module';
     StudentprofileComponent,
     AttendanceformComponent,
     AccomplishmentreportComponent,
-    ButtonsComponent
   ],
   imports: [
     CommonModule,
