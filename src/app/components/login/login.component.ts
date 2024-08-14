f9d00ff4ff1d35a25d964118dbe096dbb41b1edf
youknowmae <ganaangelmae@gmail.com>
2024-08-15 00:09:12 +0800

    revisions update

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  visible: boolean = true;
+  changetype: boolean = true;
 
+  viewpass() {
+    this.visible = !this.visible;
+    this.changetype = !this.changetype;
+  }
 }
