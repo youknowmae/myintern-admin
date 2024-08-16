efde123860fd00d6ea8dda28404089d83f23fb47
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-08-16 23:39:04 +0800

    student requirement partial connection

--- src/app/main/components/requirements/components/list/list.component.ts
+++ src/app/main/components/requirements/components/list/list.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ChangeDetectorRef  } from '@angular/core';
+
+
+import { MatPaginator, MatPaginatorIntl } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { DataService } from '../../../../../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +13,40 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
+  displayedColumns: string[] = ['name', 'student_number', 'category', 'year', 'program', 'company', 'status', 'actions'];
 
+  dataSource: any = new MatTableDataSource<any>();
+  
+  @ViewChild(MatPaginator, {static:true}) paginator!: MatPaginator;
+  
+  constructor(
+    private paginatorIntl: MatPaginatorIntl, 
+    private changeDetectorRef: ChangeDetectorRef,
+    private ds: DataService,
+    private router: Router
+  ) {
+    this.paginator = new MatPaginator(this.paginatorIntl, this.changeDetectorRef);
+  }
+
+  ngOnInit() {
+    this.getTemplates() 
+  }
+
+  getTemplates() {
+    this.ds.get('applications').subscribe(
+      templates => {
+        console.log(templates)
+        this.dataSource.data = templates;
+        this.dataSource.paginator = this.paginator;
+        console.log(this.dataSource)
+      },
+      error => {
+        console.error(error)
+      }
+    )
+  }
+
+  viewApplication(id: number) {
+    this.router.navigate(['main/requirements/view/' + id])
+  }
 }
--- src/app/main/components/requirements/requirements.module.ts
+++ src/app/main/components/requirements/requirements.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RequirementsRoutingModule } from './requirements-routing.module';
 import { ViewComponent } from './components/view/view.component';
 import { ListComponent } from './components/list/list.component';
+import { MaterialsModules } from '../../../modules/materials.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ListComponent } from './components/list/list.component';
   ],
   imports: [
     CommonModule,
-    RequirementsRoutingModule
+    RequirementsRoutingModule,
+    MaterialsModules
   ]
 })
 export class RequirementsModule { }
