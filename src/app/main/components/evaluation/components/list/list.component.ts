c928770ab30bcfa1498ca08fc3de53f4ec341710
Raveniis <128274869+Raveniis@users.noreply.github.com>
2024-08-30 14:28:30 +0800

    temporary connection

--- src/app/main/components/evaluation/components/list/list.component.ts
+++ src/app/main/components/evaluation/components/list/list.component.ts
@@ -5,6 +5,7 @@ import { MatTableDataSource } from '@angular/material/table';
 
 import { DataService } from '../../../../../services/data.service';
 import { Router } from '@angular/router';
+import { GeneralService } from '../../../../../services/general.service';
 
 @Component({
   selector: 'app-list',
@@ -25,6 +26,7 @@ export class ListComponent {
     private paginatorIntl: MatPaginatorIntl, 
     private changeDetectorRef: ChangeDetectorRef,
     private ds: DataService,
+    private gs: GeneralService,
     private router: Router
   ) {
     this.paginator = new MatPaginator(this.paginatorIntl, this.changeDetectorRef);
@@ -35,12 +37,49 @@ export class ListComponent {
   }
 
   getStudents() {
-    this.ds.get('applications').subscribe(
+    this.ds.get('evaluation/students').subscribe(
       students => {
-        console.log(students)
-        this.unfilteredStudents = students
+        console.log('test')
+        this.unfilteredStudents = students.map((student: any) => {
 
-        this.dataSource.data = students;
+          let company_name = ''
+
+          if(student.internship_applications.length > 0) {
+            company_name = student.internship_applications[0].industry_partner.company_name
+          }
+          let course = student.student_courses[0].course_code
+
+          let status 
+          var eval_average = null
+
+          if(student.student_evaluation == null) {
+            status = 'Pending'
+          } 
+          else {
+            if(student.student_evaluation.status === 1) {
+              status = 'Completed'
+              eval_average = student.student_evaluation.average
+            }
+            else {
+              var evaluation_id = student.student_evaluation.id
+              status = 'For Approval'
+            }
+          }
+          
+
+          return {
+            full_name: student.first_name + " " + student.last_name,
+            status,
+            course,
+            evaluation_id,
+            company_name,
+            eval_average,
+            ...student
+          } 
+        })
+        console.log(this.unfilteredStudents)
+
+        this.dataSource.data = this.unfilteredStudents;
         this.dataSource.paginator = this.paginator;
       },
       error => {
@@ -61,8 +100,28 @@ export class ListComponent {
     }
 
     this.dataSource.data = this.unfilteredStudents.filter((student: any) => {
-      return student.user.student_profile.program.includes(value)
+      return student.student_profile.program.includes(value)
     })
   }
 
+  approve(id: number, average: number) {
+    if(isNaN(average) || average === null) {
+      this.gs.errorAlert('Not a number!', 'The number you\'ve input is not a number')
+      return
+    }
+
+    const form = new FormData
+    form.append('average', average.toString())
+    
+    this.ds.post('evaluation/approve/', id, form).subscribe(
+      response => {
+        console.log(response)
+        this.gs.successAlert('Approved!', response.message)
+        this.getStudents()
+      },
+      error => {
+        console.error(error)
+      }
+    )
+  }
 }
