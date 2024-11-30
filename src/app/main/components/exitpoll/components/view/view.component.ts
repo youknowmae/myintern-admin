fb6b92762f2fb33d3da42caf5c86ff4be7c5ba71
youknowmae <ganaangelmae@gmail.com>
2024-12-01 00:02:53 +0800

    download

--- src/app/main/components/exitpoll/components/view/view.component.ts
+++ src/app/main/components/exitpoll/components/view/view.component.ts
@@ -1,21 +1,15 @@
 import { Component } from '@angular/core';
 import { UserService } from '../../../../../services/user.service';
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
 
-interface ExitPoll {
-  user: any,
-  short_question: any,
-  training_objectives: any,
-  likert: any,
-  long_answer: string
-}
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
-  styleUrl: './view.component.scss'
+  styleUrls: ['./view.component.scss']
 })
-
 export class ViewComponent {
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   shortAnswerQuestions = [
     'a. My scope of work is directly related to the academic program I am pursuing.',
     'b. I was given an orientation on the company organization and operations.',
@@ -24,24 +18,39 @@ export class ViewComponent {
     'e. I felt safe and secure in my work location and environment.',
     'f. I had no difficulty going to and from work.',
     'g. The company provided me with allowance, stipend, or subsidy indicate if _ _ _ meal or _ _ _ cash. If cash, how much? _ _ _/day'
-  ]
+  ];
+  exitPollDetails: any;
 
-  exitPollDetails: any
-
-  constructor(
-    private us: UserService,
-  ) {
-    this.getExitPollDetails()
+  constructor(private us: UserService) {
+    this.getExitPollDetails();
   }
 
   getExitPollDetails() {
-    var exitPollDetails = this.us.getStudentExitPoll();
-
-    this.exitPollDetails = exitPollDetails
-
-    this.exitPollDetails.industry_partner.full_address = exitPollDetails.industry_partner.street + ' ' + exitPollDetails.industry_partner.barangay + ' ' + exitPollDetails.industry_partner.municipality + ', ' + exitPollDetails.industry_partner.province
-
-    console.log(this.exitPollDetails)
+    this.exitPollDetails = this.us.getStudentExitPoll();
+    if (this.exitPollDetails) {
+      this.exitPollDetails.industry_partner.full_address =
+        `${this.exitPollDetails.industry_partner.street} ${this.exitPollDetails.industry_partner.barangay} 
+        ${this.exitPollDetails.industry_partner.municipality}, ${this.exitPollDetails.industry_partner.province}`;
+      console.log(this.exitPollDetails);
+    }
   }
 
+  downloadPdf() {
+    const doc = new jsPDF();
+
+    // Get the HTML content to be converted to PDF
+    const content = document.getElementById('formContent')!;
+
+    // Use jsPDF's html method to capture the content
+    doc.html(content, {
+      callback: (doc) => {
+        // Save the generated PDF
+        doc.save('ojt-exit-poll.pdf');
+      },
+      x: 10,
+      y: 10,
+      width: 180,  // Optional: adjust the width as needed
+      windowWidth: 800 // Optional: Adjust if the HTML content is wide
+    });
+  }
 }
